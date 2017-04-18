a2835626c7e958ba24eac8dd6d98511f8ef3276c
TheDen <deniskhoshaba@gmail.com>
2017-04-18 23:10:45 +1000

    update code

--- slackmine.go
+++ slackmine.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	"bytes"
+	"fmt"
+	"strconv"
+	"time"
+
 	redmine "github.com/mattn/go-redmine"
 	slack "github.com/nickschuch/go-slack"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"strconv"
-	"time"
 )
 
 var (
-	rmid         = kingpin.Flag("rmid", "Redmine ID. Passing the option \"me\" uses the redmine ID of the API key owner").Required().String()
+	rmid         = kingpin.Flag("rmid", "Redmine ID. Passing the option \"me\" uses the redmine ID of the API key owner").Required().OverrideDefaultFromEnvar("RM_ID").String()
 	rmkey        = kingpin.Flag("rmkey", "Redmine API key").Required().OverrideDefaultFromEnvar("RM_KEY").String()
 	slackWebhook = kingpin.Flag("slack-webhook", "Slack webhooks url").Required().OverrideDefaultFromEnvar("SLACK_WEBHOOK").String()
-	channel      = kingpin.Flag("channel", "Which slack channel to post to. Also accepts DMs, with @username").Default("#test_bots").String()
-	url          = kingpin.Flag("url", "redmine URL, e.g., https://redmine.yourcompany.com.au").String()
-	interval     = kingpin.Flag("interval", "interval check time for last ticket/issue (in minutes)").Default("5").Int()
-	botname      = kingpin.Flag("botname", "Name of the bot when it posts on Slack").Default("redminebot").String()
-	emoji        = kingpin.Flag("emoji", "Which emoji to set for the bot on Slack").Default(":monkey:").String()
+	channel      = kingpin.Flag("channel", "Which slack channel to post to. Also accepts DMs, with @username").Default("#test_bots").OverrideDefaultFromEnvar("SLACK_CHANNEL").String()
+	url          = kingpin.Flag("url", "redmine URL, e.g., https://redmine.youraccount.com.au").Default("https://redmine.com.au").OverrideDefaultFromEnvar("RM_URL").String()
+	interval     = kingpin.Flag("interval", "interval check time for last ticket/issue (in minutes)").Default("5").OverrideDefaultFromEnvar("INTERVAL_CHECK").Int()
+	botname      = kingpin.Flag("botname", "Name of the bot when it posts on Slack").Default("redmine-bot").OverrideDefaultFromEnvar("SLACK_BOTNAME").String()
+	emoji        = kingpin.Flag("emoji", "Which emoji to set for the bot on Slack").Default(":redmine:").OverrideDefaultFromEnvar("SLACK_EMOJI").String()
 )
 
 func timediff(minute_interval int) string {
@@ -28,27 +29,12 @@ func timediff(minute_interval int) string {
 }
 
 func getparams(timefrom string) string {
-	var buffer bytes.Buffer
-	buffer.WriteString(*rmkey)
-	buffer.WriteString("&assigned_to_id=")
-	buffer.WriteString(*rmid)
-	buffer.WriteString("&created_on=%3E%3D")
-	buffer.WriteString(timefrom)
-	return buffer.String()
+	return fmt.Sprintf("%s&assigned_to_id=%s&created_on=%%3E%%3D%s", *rmkey, *rmid, timefrom)
 }
 
-func sendmsg(rmsub, rmissue string) {
-	var buffer bytes.Buffer
-	buffer.WriteString("*<")
-	buffer.WriteString(*url)
-	buffer.WriteString("/issues/")
-	buffer.WriteString(rmissue)
-	buffer.WriteString("|RM")
-	buffer.WriteString(rmissue)
-	buffer.WriteString(">*\n")
-	buffer.WriteString(rmsub)
-	slack.Send(*botname, *emoji, *channel, buffer.String(), *slackWebhook)
-	buffer.Reset()
+func sendmsg(rmsub, rmissue, priority string) {
+	msg := fmt.Sprintf("*<%s/issues/%s|RM-%s>*  Priority: *%s*\n%s", *url, rmissue, rmissue, priority, rmsub)
+	slack.Send(*botname, *emoji, *channel, msg, *slackWebhook)
 }
 
 func doEvery(d time.Duration, f func(time.Time)) {
@@ -69,7 +55,7 @@ func redminecheck(t time.Time) {
 
 	for _, i := range issues {
 		if len(i.Subject) > 0 {
-			sendmsg(i.Subject, strconv.Itoa(i.Id))
+			sendmsg(i.Subject, strconv.Itoa(i.Id), i.Priority.Name)
 		}
 	}
 }
